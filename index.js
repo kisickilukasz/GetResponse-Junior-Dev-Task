c7c44fe492328de9e4bae39a6be9be8f3f679520
lukasz kisicki <lukasz.kisicki@bettersolutions.pl>
2017-04-24 12:22:49 +0200

    password placeholder type change from text to password when user start to type, made for Placeholder, so text is visible not dotted, adding class to placeholder and removing when user inputs smth, clearing of the placeholder and restoring when input value empty

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
   'use strict';
   let setCheck = null;
   const form = document.getElementById('form');
+  const ticket_count = document.getElementById('ticket-count');
   const radios = document.getElementsByName('ticket-radio');
   const inputs = document.querySelectorAll('[data-placeholder]');
-  console.log(inputs);
 
   // logic for unchecking previously checked radio input
   radios.forEach((radio) => {
     radio.onclick = () => {
       if (setCheck != radio) {
         setCheck = radio;
+        ticket_count.value = radio.value;
       } else {
         radio.checked = false;
         setCheck = null;
+        ticket_count.value = null;
       }
     }
   });
@@ -22,38 +24,85 @@
   inputs.forEach((input) => {
     if (!input.value.lenght) {
       input.value = input.dataset.placeholder;
+
       if (input.type === "password") {
         input.type = "text";
       }
-    } else {
-      input.value = input.value;
-    };
+    }
+    return;
   });
 
   // sets caret at the beginning of an input
   const setCaret = event => {
     let element = event.target;
+
     if (element.value === element.getAttribute("data-placeholder")) {
       element.setSelectionRange(0, 0);
-      event.preventDefault();
-      event.stopPropagation();
+
+      if (event.keyCode !== 9) {        // for the user that is navigating through the form with tab key
+        event.preventDefault();
+        event.stopPropagation();
+      }
       return false;
     }
   }
 
   const clearPlaceholder = event => {
+    let element = event.target;
+
+    if (event.keyCode === 9) {
+      return;
+    }
+
+    if (element.value === element.getAttribute("data-placeholder")) {
+      setPasswordInputType(element);
+      element.value = "";
+      removeClassFromPlaceholder(element);
+    }
     console.log(event);
   }
 
   const restorePlaceholder = event => {
+    let element = event.target;
+
+    if (element.value !== element.getAttribute("data-placeholder") && element.value === "") {
+      setPasswordInputType(element);
+      element.value = element.getAttribute("data-placeholder");
+      addClassToPaceholder(element);
+      setCaret(event);
+    }
+
+    setCaret(event);
     console.log(event);
   }
 
+  const setPasswordInputType = element => {
+    if (element.getAttribute("data-placeholder") === "Password" && element.value === "") {
+      element.type = "text";
+    };
+
+    if (element.getAttribute("data-placeholder") === "Password" && element.value !== "") {
+      element.type = "password";
+    };
+  }
+
+  const addClassToPaceholder = element => {
+    if (element.name === "ticket_count") {
+      return
+    } else {
+      element.classList.add("input-placeholder");
+    }
+  }
+
+  const removeClassFromPlaceholder = element => {
+    element.classList.remove("input-placeholder");
+  }
+
+  // event delegation on form
   form.addEventListener("focus", setCaret, false);
-  form.addEventListener("drop", setCaret, false);
   form.addEventListener("click", setCaret, false);
   form.addEventListener("keydown", clearPlaceholder, false);
   form.addEventListener("keyup", restorePlaceholder, false);
-  form.addEventListener("blur", restorePlaceholder, false);
+  form.addEventListener("blur", restorePlaceholder, true);
 
 })(window)
